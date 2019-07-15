a0e3fbdacfd2b6696c746a99942bc087e58086be
Grimmino <Grimmino@gmail.com>
2019-07-15 15:52:58 +0500

    deleted trash

--- static/index.js
+++ static/index.js
@@ -1,19 +1,19 @@
 //scss
-import './sass/style.sass';
+import './sass/style.sass'
 //jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 //components
-import Datepicker from "./components/datepicker/Datepicker";
+//import Datepicker from "./components/datepicker/Datepicker";
 //js
-require('./js/header'); //js для header
-require('./js/app');
-require('./js/sidebar');
-require('./js/sw-main-js');
-require('./js/slider');
-require('./js/Desk');
+import './js/header'
+//import './js/app'
+//import './js/sidebar'
+//import './js/sw-main-js'
+//import './js/slider'
+//import './js/Desk'
 //datepicker
-require('./js/vendor/airdatepicker');
-require('./js/datepicker');
+//import './js/vendor/airdatepicker'
+//import './js/datepicker'
 
-ReactDOM.render(<Datepicker />, document.getElementById('sw__datepicker'));
+//ReactDOM.render(<Datepicker />, document.getElementById('sw__datepicker'));
