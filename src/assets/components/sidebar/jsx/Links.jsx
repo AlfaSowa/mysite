deb5e71cbc9576b1c79d64fbe9c8133cc378c11a
Sowa <Grimmino@gmail.com>
2020-05-25 17:27:01 +0500

    rebuild

--- src/assets/components/sidebar/jsx/Links.jsx
+++ src/assets/components/sidebar/jsx/Links.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export const Links = (props) => {
     return (
-        <div className='board__sidebar-item refs'>
-            <ul className='refs__list'>
+        <div className="board__sidebar-item refs">
+            <ul className="refs__list">
                 {props.links.map((item, index) => (
-                    <li key={index} className='refs__item'>
+                    <li key={index} className="refs__item">
                         <span>{item.title}</span>
-                        <a className='refs__link' href={item.link} target='_blank'>
+                        <a className="refs__link" href={item.link} target="_blank">
                             открыть
-                            <svg className='refs__icon'>
-                                <use xlinkHref='#arrow-right'></use>
+                            <svg className="refs__icon">
+                                <use xlinkHref="#arrow-right"></use>
                             </svg>
                         </a>
                     </li>
@@ -23,20 +23,20 @@ export const Links = (props) => {
 Links.defaultProps = {
     links: [
         {
-            title: 'Мое резюме на hh',
-            link: 'https://hh.ru/resume/ffd982adff015f658b0039ed1f79534c574879'
+            title: "Мое резюме на hh",
+            link: "https://hh.ru/resume/ffd982adff015f658b0039ed1f79534c574879",
         },
         {
-            title: 'Мое резюме на LinkedIn',
-            link: 'https://www.linkedin.com/in/alexey-tamson-48ba51194/'
+            title: "Мое резюме на LinkedIn",
+            link: "https://www.linkedin.com/in/alexey-tamson-48ba51194/",
         },
         {
-            title: 'Мой репозиторий на GitHub',
-            link: 'https://github.com/Grimmino'
+            title: "Мой репозиторий на GitHub",
+            link: "https://github.com/AlfaSowa",
         },
         {
-            title: 'Мой профиль на CodeWars',
-            link: 'https://www.codewars.com/users/Grimmino'
-        }
-    ]
+            title: "Мой профиль на CodeWars",
+            link: "https://www.codewars.com/users/AlfaSowa",
+        },
+    ],
 };
